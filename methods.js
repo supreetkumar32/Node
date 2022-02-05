293c4c789e641394d4b7b36d0c1d4c6765c8cdc4
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2022-02-06 00:11:16 +0530

    Update methods.js

--- methods.js
+++ methods.js
@@ -5,17 +5,15 @@ app.use(express.json());
 let users={};
 app.listen(3000);
 
-
-
 app.get('/users',(req,res)=>{
     res.send(users);
 })
 
 app.post('/users',(req,res)=>{
     console.log(req.body);
-    users=req.body;
+    users=req.body;                          
     res.json({
         message:"data received successfully",  //data received successfully postman ke niche print hoga
-        user:req.body                           //postman jo data entry karenge wo iake console mey print hoga req.boody ke help se
+        user:req.body                           //postman jo data entry karenge wo iske console mey print hoga req.body ke help se
     });
 });
