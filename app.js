4ee55e4ad338b6d7877d160eff5b4133fe9bedcf
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2022-02-05 23:20:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ app.get('/',function(req,res){
 app.get('/about',function(req,res){
     res.sendFile('./views/about.html',{root:__dirname});
 });
+
+///redirects
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about');
+});
+
+//404 page
+app.use((req,res)=>{
+    res.status(404).sendFile('./views/404.html',{root:__dirname});
+})
