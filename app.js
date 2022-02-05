944c3745142e4069717982fbeaf29494ad4286bf
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2022-02-05 23:12:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express=require('express');
 const app=express();
 app.listen(3000);
+
+// app.get('/',function(req,res){
+//     res.send('Hello World')
+// });
+
+// app.get('/about',function(req,res){
+//     res.send('About Page')
+// });
+
 app.get('/',function(req,res){
-    res.send('Hello World')
+    res.sendFile('C:/Users/supre/Desktop/nodelast/views/index.html')
 });
 
 app.get('/about',function(req,res){
-    res.send('About Page')
+    res.sendFile('./views/about.html',{root:__dirname});
 });
