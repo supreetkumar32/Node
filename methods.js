8f4ddf94ae2da5d60352432e13f1ee348f796760
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2022-02-06 00:09:56 +0530

    Create methods.js

---
+++ methods.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+//middleware
+app.use(express.json());
+let users={};
+app.listen(3000);
+
+
+
+app.get('/users',(req,res)=>{
+    res.send(users);
+})
+
+app.post('/users',(req,res)=>{
+    console.log(req.body);
+    users=req.body;
+    res.json({
+        message:"data received successfully",  //data received successfully postman ke niche print hoga
+        user:req.body                           //postman jo data entry karenge wo iake console mey print hoga req.boody ke help se
+    });
+});
